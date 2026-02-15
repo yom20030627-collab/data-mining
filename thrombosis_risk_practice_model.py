5e1391f4db03cd741f98c4b28763bd168eb5830b
yom20030627-collab <yom20030627@gmail.com>
2026-02-15 19:32:20 +0800

    Update files

--- thrombosis_risk_practice_model.py
+++ thrombosis_risk_practice_model.py
@@ -2,18 +2,22 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("simulated_hospital_large_dataset.csv")
-df["obesity"] = (df["BMI"] >= 27).astype(int)
-df["HTN-DM"] = df["hypertension"] * df["diabetes"]
-df["obesity-HTN"] = df["obesity"] * df["hypertension"]
-df["obesity-DM"] = df["obesity"] * df["diabetes"]
-df["obesity-HTN-DM"] = df["obesity"] * df["hypertension"] * df["diabetes"]
-df["obesity-HTN-DM-CKD"] = df["obesity"] * df["hypertension"] * df["diabetes"] * df["CKD"]
-df["obesity-HTN-DM-CKD-CAD"] = df["obesity"] * df["hypertension"] * df["diabetes"] * df["CKD"] * df["CAD"]
+df ["obesity"] = (df["BMI"] >= 27).astype(int)
 
-features = ["age", "BMI", "smoking", "alcohol", "hypertension", "diabetes", "hyperlipidemia", "CKD", "obesity","CAD", "stroke_history", "HTN-DM", "obesity-HTN", "obesity-DM", "obesity-HTN-DM", "obesity-HTN-DM-CKD", "obesity-HTN-DM-CKD-CAD"]
+np.random.seed(42)
+
+risk_score = (1.5*df["hypertension"] + 1.8*df["diabetes"] + 1.3*df["obesity"] + 0.5*df["hyperlipidemia"] + 0.5*df["CKD"] + 0.5*df["CAD"] + 0.5*df["stroke_history"] + 0.7*df["smoking"] + 0.3*df["alcohol"] + 0.02*df["age"])
+risk_score += 0.8 * (df["hypertension"] * df["diabetes"])  # Interaction effect
+noise = np.random.normal(0, 1, size=len(df))
+risk_score += 3 * noise  # Add noise to make it more realistic
+prob = 1 / (1 + np.exp(-risk_score))
+df["thrombosis_event"] = np.random.binomial(1, prob)
+df["HTN-DM"] = df["hypertension"] * df["diabetes"]
+
+features = ["age","HTN-DM","BMI", "smoking", "alcohol", "hypertension", "diabetes", "hyperlipidemia", "CKD", "obesity","CAD", "stroke_history"]
 
 X = df[features]
-y = df["mortality"]
+y = df["thrombosis_event"]
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -25,6 +29,10 @@ from sklearn.metrics import classification_report, f1_score, roc_auc_score, clas
 
 model = RandomForestClassifier(n_estimators=200, max_depth=6, random_state=42, class_weight='balanced')
 model.fit(X_train, y_train)
+
+from sklearn.linear_model import LogisticRegression
+log_model = LogisticRegression(max_iter=1000, class_weight='balanced')
+log_model.fit(X_train, y_train)
 
 y_prob = model.predict_proba(X_test)[:, 1]  # Probability of positive class (mortality)
 best_f1 = 0
@@ -45,6 +53,8 @@ print(classification_report(y_test, y_pred))
 print("ROC AUC Score:", roc_auc_score(y_test, y_prob))
 print("Best Threshold:", best_threshold)
 print("Best F1 Score:", best_f1)
+print(df["thrombosis_event"].value_counts(normalize=True))
+print("Logistic Regression ROC AUC Score:", roc_auc_score(y_test, log_model.predict_proba(X_test)[:, 1]))
 
 from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
